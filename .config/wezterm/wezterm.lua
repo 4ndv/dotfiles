8f513b2250e49a5aadf49b7bc9ff9cfa5323120a
Andrey Viktorov <andreyviktorov@outlook.com>
2024-03-24 15:07:13 +0700

    feat: Opt+Left/Opt+Right for navigating words in wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,8 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'Fira Code'
 config.font_size = 18.0
 
+config.color_scheme = 'Dark Pastel'
+
 config.keys = {
   -- iTerm2-like scrollback clear (Windows/Linux)
   {
@@ -24,6 +26,22 @@ config.keys = {
       act.SendKey { key = 'L', mods = 'CTRL' },
     },
   },
+  -- Option+Left for move backwards by a word
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act {
+      SendString = "\x1bb"
+    }
+  },
+  -- Option+Right for move forwards by a word
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act {
+      SendString = "\x1bf"
+    }
+  }
 }
 
 return config
