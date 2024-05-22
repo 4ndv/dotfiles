2d35137e5aba506c7b994d504c988694d73dce47
Andrey Viktorov <andreyviktorov@outlook.com>
2024-05-23 00:38:37 +0700

    feat(linux): wezterm correct font sizes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,17 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = wezterm.config_builder()
 
+local is_linux = function()
+  return wezterm.target_triple:find("linux") ~= nil
+end
+
+if is_linux() then
+  config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+end
+
 config.font = wezterm.font 'Fira Code'
-config.font_size = 18.0
+-- On Linux font_size is weirdly affected by scaling
+config.font_size = is_linux() and 14.0 or 18.0
 
 config.color_scheme = 'Dark Pastel'
 
