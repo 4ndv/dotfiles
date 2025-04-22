f6f55f41feaf7e78836acdc9ef17575eb296f12f
Andrey Viktorov <loonks@disroot.org>
2025-04-23 00:00:38 +0700

    fish: orbstack stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -46,3 +46,7 @@ if status is-interactive
         eval (zellij setup --generate-auto-start fish | string collect)
     end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
